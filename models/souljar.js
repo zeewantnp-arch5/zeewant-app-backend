5306e9c67019f09e1c9a36be712fcd85b235a2dc
Dileep <dileepkushwaha2222@gmail.com>
2026-05-10 13:36:47 +0545

    modified in souljar.js

--- models/souljar.js
+++ models/souljar.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const souljarSchema = new mongoose.Schema(
   {
     userId: {
-      type: mongoose.Schema.Types.ObjectId,
+      type: String,
       required: true,
     },
 
